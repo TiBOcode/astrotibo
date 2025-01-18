b559c50fc13e2c1db68bee84c52bb7aae2910573
tibo <132303217+TiBOcode@users.noreply.github.com>
2025-01-18 01:01:48 +0100

    First

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vue from '@astrojs/vue';
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeRaw from 'rehype-raw';
 
-
-
-export default defineConfig({
-  output: 'static',
-  integrations: [tailwind(), vue()],
-
-  content: {
-    collections: {
-      notes: {
-        schema: ({ z }) => z.object({
-          title: z.string(),
-          date: z.coerce.date().optional(),
-          description: z.string().optional().default(''),
-        })
-      }
-    }
-  },
-
-  markdown: {
-    extendDefaultPlugins: true,
-    
-    rehypePlugins: [
-      [rehypeExternalLinks, {
-        target: '_blank',
-        rel: ['noopener', 'noreferrer'],
-      }]
-    ]
-  }
-});
+// https://astro.build/config
+export default defineConfig({});
